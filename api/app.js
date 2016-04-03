30e143618ac069dc448002d681d1280c64513bca
Gregory Daynes <gregdaynes@gmail.com>
2016-04-03 15:31:33 -0700

    consistency is key

--- api/app.js
+++ api/app.js
@@ -25,10 +25,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Attach routes ==============
 // allow CORS
 app.all('*', (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
     res.header('Access-Control-Allow-Headers', 'Content-type,Accept,X-Access-Token,X-Key');
-    if (req.method == 'OPTIONS') res.status(200).end();
+    if (req.method === 'OPTIONS') res.status(200).end();
     else next();
 });
 
